d0cdd62cf7bcb59a3cf471ae64e65710749f77cd
tanyixiu <yixiu.tan@gmail.com>
2019-08-03 09:56:10 +0800

    [Yixiu] refactor aws function

--- aws/aws_ci.sh
+++ aws/aws_ci.sh
@@ -1,58 +1,65 @@
 #!/usr/bin/env bash
 
-current_script_dir="$( cd "$(dirname "$0")" ; pwd -P )"
-
+local current_script_dir="$( cd "$(dirname "$0")" ; pwd -P )"
 source ${current_script_dir}/connection.sh
 
-function get_ci_server_ip {
-  echo "Getting AWS instance CI-Server IP ......"
-  get_aws_public_ip_by_name CI-Server
+function get_aws_instance_ip {
+  local instance_name=$1
+  echo "Getting AWS instance ${instance_name} IP ......"
+  get_aws_public_ip_by_name ${instance_name}
 }
 
-function get_ci_server_status {
-  echo "Getting AWS instance CI-Server status ......"
-  get_aws_instance_status_by_name CI-Server
+function get_aws_instance_status {
+  local instance_name=$1
+  echo "Getting AWS instance ${instance_name} status ......"
+  get_aws_instance_status_by_name ${instance_name}
 }
 
-function start_aws_ci_server {
-  echo "Starting AWS instance CI-Server ......"
-  start_aws_instance_by_name CI-Server
+function start_aws_instance {
+  local instance_name=$1
+  echo "Starting AWS instance ${instance_name} ......"
+  start_aws_instance_by_name ${instance_name}
 }
 
-function stop_aws_ci_server {
-  echo "Stopping AWS instance CI-Server ......"
-  stop_aws_instance_by_name CI-Server
+function stop_aws_instance {
+  local instance_name=$1
+  echo "Stopping AWS instance ${instance_name} ......"
+  stop_aws_instance_by_name ${instance_name}
 }
 
-function ssh_aws_ci_server {
-  echo "Login to AWS CI-Server ......"
-  ssh_into_aws_instance CI-Server
+function ssh_aws_instance {
+  local instance_name=$1
+  echo "Login to AWS ${instance_name} ......"
+  ssh_into_aws_instance ${instance_name}
 }
 
-function aws_ci {
-  param=$1
+function my_aws {
+  local param=$1
+  local instance_name=$2
 
   case "${param}" in
     "status")
-      get_ci_server_status
+      get_aws_instance_status ${instance_name}
       ;;
     "start")
-      start_aws_ci_server
+      start_aws_instance ${instance_name}
       ;;
     "stop")
-      stop_aws_ci_server
+      stop_aws_instance ${instance_name}
       ;;
     "ssh")
-      ssh_aws_ci_server
+      ssh_aws_instance ${instance_name}
       ;;
     "ip")
-      get_ci_server_ip
+      get_aws_instance_ip ${instance_name}
       ;;
   esac
 }
 
-alias aws.ci.status=get_ci_server_status
-alias aws.ci.start=start_aws_ci_server
-alias aws.ci.stop=stop_aws_ci_server
-alias aws.ci.ssh=ssh_aws_ci_server
-alias aws.ci.ip=get_ci_server_ip
+AWS_CI_INSTANCE_NAME="CI-Server"
+
+alias aws.ci.status="my_aws status ${AWS_CI_INSTANCE_NAME}"
+alias aws.ci.start="my_aws start ${AWS_CI_INSTANCE_NAME}"
+alias aws.ci.stop="my_aws stop ${AWS_CI_INSTANCE_NAME}"
+alias aws.ci.ssh="my_aws ssh ${AWS_CI_INSTANCE_NAME}"
+alias aws.ci.ip="my_aws ip ${AWS_CI_INSTANCE_NAME}"
--- aws/connection.sh
+++ aws/connection.sh
@@ -1,66 +1,66 @@
 #!/usr/bin/env bash
 
 function get_aws_instance_info_by_name {
-  instance_name=$1
+  local instance_name=$1
 
-  filed_name=$2
+  local filed_name=$2
 
-  aws_instance_info=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${instance_name}" --query "Reservations[0].Instances[0].${filed_name}" --output text | awk -F\" '{print $1}')
+  local aws_instance_info=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${instance_name}" --query "Reservations[0].Instances[0].${filed_name}" --output text | awk -F\" '{print $1}')
 
   echo ${aws_instance_info}
 }
 
 function get_aws_instance_id_by_name {
-  instance_name=$1
+  local instance_name=$1
 
-  instance_id=$(get_aws_instance_info_by_name ${instance_name} "InstanceId")
+  local instance_id=$(get_aws_instance_info_by_name ${instance_name} "InstanceId")
 
   echo ${instance_id}
 }
 
 function get_aws_public_ip_by_name {
-  instance_name=$1
+  local instance_name=$1
 
-  ip_address=$(get_aws_instance_info_by_name ${instance_name} "PublicIpAddress")
+  local ip_address=$(get_aws_instance_info_by_name ${instance_name} "PublicIpAddress")
 
   echo ${ip_address}
 }
 
 function get_aws_instance_key_by_name {
-  instance_name=$1
+  local instance_name=$1
 
-  key_name=$(get_aws_instance_info_by_name ${instance_name} "KeyName")
+  local key_name=$(get_aws_instance_info_by_name ${instance_name} "KeyName")
 
   echo ${key_name}
 }
 
 function get_aws_instance_status_by_name {
-  instance_name=$1
+  local instance_name=$1
 
-  state=$(get_aws_instance_info_by_name ${instance_name} "State.Name")
+  local state=$(get_aws_instance_info_by_name ${instance_name} "State.Name")
 
   echo ${state}
 }
 
 function ssh_into_aws_instance {
-  instance_name=$1
+  local instance_name=$1
 
-  ip_address=$(get_aws_public_ip_by_name ${instance_name})
+  local ip_address=$(get_aws_public_ip_by_name ${instance_name})
 
-  key_name=$(get_aws_instance_key_by_name ${instance_name})
+  local key_name=$(get_aws_instance_key_by_name ${instance_name})
 
   ssh -i ~/Scripts/AWS/Keys/${key_name}.pem -o StrictHostKeyChecking=no ec2-user@${ip_address}
 }
 
 function start_aws_instance_by_name {
-  instance_name=$1
-  instance_id=$(get_aws_instance_id_by_name ${instance_name})
+  local instance_name=$1
+  local instance_id=$(get_aws_instance_id_by_name ${instance_name})
   aws ec2 start-instances --instance-ids ${instance_id}
 }
 
 function stop_aws_instance_by_name {
-  instance_name=$1
-  instance_id=$(get_aws_instance_id_by_name ${instance_name})
+  local instance_name=$1
+  local instance_id=$(get_aws_instance_id_by_name ${instance_name})
   aws ec2 stop-instances --instance-ids ${instance_id}
 }
 
